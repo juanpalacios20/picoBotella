02d88bcdf2eb3ebaa96fdbd6ec5178d729361ad1
Kevin Felipe Bolaños Orozco <166841728+FelipeK57@users.noreply.github.com>
2024-10-22 11:10:38 -0500

    Add action to review button for link with link into play store to the nequi app

--- app/src/main/java/com/example/picobotella/HomeActivity.kt
+++ app/src/main/java/com/example/picobotella/HomeActivity.kt
@@ -1,16 +1,31 @@
 package com.example.picobotella
 
+import android.media.Image
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.CountDownTimer
 import android.view.animation.AnimationUtils
 import android.widget.ImageButton
 import android.widget.TextView
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+
+        val ReviewButton = findViewById<ImageButton>(R.id.icon_star)
+
+        ReviewButton.setOnClickListener {
+            val url = "https://play.google.com/store/apps/details?id=com.nequi.MobileApp&hl=es_CO&pli=1"
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(url)
+            }
+            startActivity(intent)
+        }
 
         val pressMeButton: ImageButton = findViewById(R.id.btnPressMe)
         val blinkAnimation = AnimationUtils.loadAnimation(this, R.anim.blink)
